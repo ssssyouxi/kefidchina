fd45f49a6c5326ec32ba5599481f77a950b06808
ssssyouxi <361490334@qq.com>
2016-11-17 15:20:30 +0800

    news page complete

--- js/news.js
+++ js/news.js
@@ -3,18 +3,26 @@ $(function(){
 			if(obj < 1000){
 			$(".news_title").removeClass("float-right");
 			$(".news_img").addClass("img-responsive");
+			$("#previous1").html("Previous");
+			$("#next1").html("Next")
 		}else{
 			$(".news_title").addClass('float-right');
-			$(".news_img").removeClass("img-responsive")
+			$(".news_img").removeClass("img-responsive");
+			$("#previous1").html("Previous: Kefid's tailings processing system is ...");
+			$("#next1").html("Next: Kefid's tailings processing system is ...")
 		};
 	$(window).resize(function(){
 		var obj=$(window).width();
 		if(obj < 1000){
 			$(".news_title").removeClass("float-right");
 			$(".news_img").addClass("img-responsive");
+			$("#previous1").html("Previous");
+			$("#next1").html("Next");
 		}else{
 			$(".news_title").addClass('float-right');
-			$(".news_img").removeClass("img-responsive")
+			$(".news_img").removeClass("img-responsive");
+			$("#previous1").html("Previous: Kefid's tailings processing system is ...");
+			$("#next1").html("Next: Kefid's tailings processing system is ...")
 		};
 	})
 })
