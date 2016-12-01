ab0732e8ba23a5d251bcff0b74f8f570b2e68ed3
ssssyouxi <361490334@qq.com>
2016-12-01 10:24:17 +0800

    fix a bug about line-height that when solution.html loading complete,line-height is 0

--- js/solution.js
+++ js/solution.js
@@ -1,6 +1,12 @@
-$(function(){
-	var obj = $(window).width();
-	if(obj<760){
-		$(".solution_flo").width()=$(".img-responsive").width()
-	}
-})
+window.onload=function(){
+		var w=$(".img-responsive").width();
+		$(".solution_flo").width(w);
+		var pl=($(".solution_div").width()-$(".img-responsive").width())*0.5;
+		$(".img-responsive").css('padding-left', pl);
+		$(".solution_flo").css('margin-left',pl);
+		var p_lh=$(".solution_flo").height();
+		if(p_lh==0){
+			p_lh=66;
+		}
+		$(".solution_flo p").css('line-height',p_lh+"px");
+}
